a3eeaf73ae454be41325630f6f60441d65b2d778
unknown <17319296359@163.com>
2019-04-13 14:03:08 +0800

    一些常用的文件处理/文件转换的工具脚本

--- Tools/proxy/get_proxies.py
+++ Tools/proxy/get_proxies.py
@@ -7,8 +7,8 @@ def get_proxies():
     连接API获取付费的代理IP
     :return: 代理IP
     """
-    # 鹏鹏代理池的接口
-    url = 'http://39.107.59.59/get'
+    # 代理池的接口
+    url = 'proxy_address'
     # 发送请求获取响应
     results = json.loads(requests.get(url).text)['RESULT']
     print("查看获取的响应：", results)
